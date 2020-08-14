9607e858473138af53b4112305cc2f7e880e8777
Simon A. F. Lund <simon.lund@samsung.com>
2020-08-14 14:32:44 +0200

    envs
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- envs/refenv-xnvme-qemu.sh
+++ envs/refenv-xnvme-qemu.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
-# CIJOE: SSH_* environment variables
-: "${SSH_HOST=localhost}"; export SSH_HOST
-: "${SSH_PORT=2222}"; export SSH_PORT
-: "${SSH_USER=root}"; export SSH_USER
-: "${SSH_NO_CHECKS=1}"; export SSH_NO_CHECKS
+#
+# QEMU target environment skeleton
+#
+# The SSH configuration is overwritten such that everything using the "ssh::"
+# module will be targeted towards the QEMU guest.
+#
+# To access the QEMU host an explicit "QEMU_HOST" and utility functions are
+# provided by the "qemu::" module
+#
 
 # CIJOE: QEMU_* environment variables
 : "${QEMU_HOST:=localhost}"; export QEMU_HOST
@@ -12,9 +15,21 @@
 : "${QEMU_HOST_PORT:=22}"; export QEMU_HOST_PORT
 : "${QEMU_HOST_SYSTEM_BIN:=/opt/qemu/x86_64-softmmu/qemu-system-x86_64}"; export QEMU_HOST_SYSTEM_BIN
 : "${QEMU_HOST_IMG_BIN:=qemu-img}"; export QEMU_HOST_IMG_BIN
-: "${QEMU_GUEST_SSH_FWD_PORT:=$SSH_PORT}"; export QEMU_GUEST_SSH_FWD_PORT
+
 : "${QEMU_GUESTS:=/opt/guests}"; export QEMU_GUESTS
 : "${QEMU_GUEST_NAME:=emujoe}"; export QEMU_GUEST_NAME
+: "${QEMU_GUEST_SSH_FWD_PORT:=2222}"; export QEMU_GUEST_SSH_FWD_PORT
+: "${QEMU_GUEST_CONSOLE:=file}"; export QEMU_GUEST_CONSOLE
+: "${QEMU_GUEST_MEM:=4G}"; export QEMU_GUEST_MEM
+: "${QEMU_GUEST_SMP:=4}"; export QEMU_GUEST_SMP
+#: "${QEMU_GUEST_KERNEL:=1}"; export QEMU_GUEST_KERNEL
+#: "${QEMU_GUEST_APPEND:=net.ifnames=0 biosdevname=0}"; export QEMU_GUEST_APPEND
+
+# CIJOE: SSH_* environment variables
+: "${SSH_HOST:=localhost}"; export SSH_HOST
+: "${SSH_PORT:=$QEMU_GUEST_SSH_FWD_PORT}"; export SSH_PORT
+: "${SSH_USER:=root}"; export SSH_USER
+: "${SSH_NO_CHECKS:=1}"; export SSH_NO_CHECKS
 
 #
 # xNVMe 1/2: set PCI_DEV_NAME, NVME_CNTID, and NVME_NSID based on NVME_NSTYPE
