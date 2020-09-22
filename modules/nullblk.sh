11c09cad10d148087309b892decf28624a5ea12c
Simon A. F. Lund <simon.lund@samsung.com>
2020-09-22 19:56:43 +0200

    mod:nullblk: added option to create instances
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- modules/nullblk.sh
+++ modules/nullblk.sh
@@ -23,15 +23,18 @@ nullblk::env() {
 
   : "${NULLBLK_MODULE_NAME:=null_blk}"
 
+  # Setup via cfg-path
   : "${NULLBLK_QUEUE_MODE:=2}"
   : "${NULLBLK_HOME_NODE:=0}"
-  : "${NULLBLK_GB:=100}"
   : "${NULLBLK_BS:=512}"
-  : "${NULLBLK_NR_DEVICES:=1}"
   : "${NULLBLK_IRQMODE:=2}"
+
+  : "${NULLBLK_GB:=14}"
+  : "${NULLBLK_NR_DEVICES:=1}"
   : "${NULLBLK_COMPLETION_NSEC:=10}"
   : "${NULLBLK_SUBMIT_QUEUES:=1}"
   : "${NULLBLK_HW_QUEUE_DEPTH:=64}"
+  : "${NULLBLK_MEMORY_BACKED:=1}"
 
   # Multi-queue stuff
   : "${NULLBLK_USE_PER_NODE_HCTX:=0}"
@@ -43,6 +46,13 @@ nullblk::env() {
   : "${NULLBLK_ZONED:=0}"
   : "${NULLBLK_ZONE_SIZE:=256}"         # Size in units of MB
   : "${NULLBLK_ZONE_NR_CONV:=0}"        # Number of conventional / random-access zones
+}
+
+nullblk::params() {
+  if ! nullblk::env; then
+    cij::err "nullblk::env - invalid Null Block ENV."
+    return 1
+  fi
 
   NULLBLK_PARAMS=""
   NULLBLK_PARAMS="${NULLBLK_PARAMS} queue_mode=${NULLBLK_QUEUE_MODE}"
@@ -54,6 +64,7 @@ nullblk::env() {
   NULLBLK_PARAMS="${NULLBLK_PARAMS} completion_nsec=${NULLBLK_COMPLETION_NSEC}"
   NULLBLK_PARAMS="${NULLBLK_PARAMS} submit_queues=${NULLBLK_SUBMIT_QUEUES}"
   NULLBLK_PARAMS="${NULLBLK_PARAMS} hw_queue_depth=${NULLBLK_HW_QUEUE_DEPTH}"
+  NULLBLK_PARAMS="${NULLBLK_PARAMS} memory_hw_queue_depth=${NULLBLK_HW_QUEUE_DEPTH}"
 
   NULLBLK_PARAMS="${NULLBLK_PARAMS} use_per_node_hctx=${NULLBLK_USE_PER_NODE_HCTX}"
   NULLBLK_PARAMS="${NULLBLK_PARAMS} no_sched=${NULLBLK_NO_SCHED}"
@@ -63,15 +74,100 @@ nullblk::env() {
   NULLBLK_PARAMS="${NULLBLK_PARAMS} zoned=${NULLBLK_ZONED}"
   NULLBLK_PARAMS="${NULLBLK_PARAMS} zone_size=${NULLBLK_ZONE_SIZE}"
   NULLBLK_PARAMS="${NULLBLK_PARAMS} zone_nr_conv=${NULLBLK_ZONE_NR_CONV}"
+
   export NULLBLK_PARAMS
 
   return 0
 }
 
-nullblk::insert() {
+nullblk::create() {
   if ! nullblk::env; then
     cij::err "nullblk::env - invalid Null Block ENV."
     return 1
+  fi
+
+  local _kpath="/sys/kernel/config/nullb"
+  local _size
+  local _id=0
+  local _limit=0
+  local _cfg_path
+  local _dev_path
+
+  while [[ _limit -lt 10 ]]; do
+    _cfg_path="${_kpath}/nullb${_id}"
+    _dev_path="/dev/nullb${_id}"
+
+    if ssh::cmd "mkdir ${_cfg_path}"; then
+      break
+    fi
+
+    _limit=$(( _limit + 1 ))
+    _id=$(( _id + 1 ))
+  done
+
+  if ! ssh::cmd "echo '${NULLBLK_HW_QUEUE_DEPTH}' >> ${_cfg_path}/hw_queue_depth"; then
+    return 1
+  fi
+  if ! ssh::cmd "echo '${NULLBLK_HOME_NODE}' >> ${_cfg_path}/home_node"; then
+    return 1
+  fi
+  if ! ssh::cmd "echo '${NULLBLK_QUEUE_MODE}' >> ${_cfg_path}/queue_mode"; then
+    return 1
+  fi
+  if ! ssh::cmd "echo '${NULLBLK_BS}' >> ${_cfg_path}/blocksize"; then
+    return 1
+  fi
+
+  _size=$(( NULLBLK_GB * 1024 ))
+  if ! ssh::cmd "echo '${_size}' >> ${_cfg_path}/size"; then
+    return 1
+  fi
+
+  if ! ssh::cmd "echo '${NULLBLK_COMPLETION_NSEC}' >> ${_cfg_path}/completion_nsec"; then
+    return 1
+  fi
+  if ! ssh::cmd "echo '${NULLBLK_IRQMODE}' >> ${_cfg_path}/irqmode"; then
+    return 1
+  fi
+  if ! ssh::cmd "echo '${NULLBLK_BLOCKING}' >> ${_cfg_path}/blocking"; then
+    return 1
+  fi
+
+  if ! ssh::cmd "echo '${NULLBLK_MEMORY_BACKED}' >> ${_cfg_path}/memory_backed"; then
+    return 1
+  fi
+
+  if ! ssh::cmd "echo '${NULLBLK_ZONED}' >> ${_cfg_path}/zoned"; then
+    return 1
+  fi
+  if [[ -v NULLBLK_ZONED && NULLBLK_ZONED == "1" ]]; then
+    if ! ssh::cmd "echo '${NULLBLK_ZONE_SIZE}' >> ${_cfg_path}/zone_size"; then
+      return 1
+    fi
+    if ! ssh::cmd "echo '${NULLBLK_ZONE_NR_CONV}' >> ${_cfg_path}/zone_nr_conv"; then
+      return 1
+    fi
+  fi
+
+  if ssh::cmd "echo '1' >> ${_cfg_path}/power"; then
+    return 1
+  fi
+
+  return 0
+}
+
+nullblk::insert() {
+  if ! nullblk::params; then
+    cij::err "nullblk::env - invalid params"
+    return 1
+  fi
+
+  # Create Null-block instances via config-fs instead
+  if [[ "${NULLBLK_NR_DEVICES}" == "0" ]]; then
+    NULLBLK_PARAMS="nr_devices=0"
+    cij::info "nullblk::insert: nr_devices: 0; expecting config via cfgfs"
+  else
+    cij::info "nullblk::insert: nr_devices: 0 > 0; doing stuff"
   fi
 
   if ! ssh::cmd "modprobe ${NULLBLK_MODULE_NAME} ${NULLBLK_PARAMS}"; then
@@ -88,8 +184,11 @@ nullblk::remove() {
     return 1
   fi
 
+  if ! ssh::cmd "rmdir /sys/kernel/config/nullb/nullb*"; then
+    cij::info "nullblk:::remove: failed removing instances"
+  fi
   if ! ssh::cmd "modprobe -r ${NULLBLK_MODULE_NAME}"; then
-    cij::err "nullblk:::insert: failed modprobe"
+    cij::err "nullblk:::remove: failed removing module"
     return 1
   fi
 
