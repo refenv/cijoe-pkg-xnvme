4e9d412c6e82ac1c9a68c1f4dc95d96bc94c2215
Simon A. F. Lund <simon.lund@samsung.com>
2020-09-22 20:20:29 +0200

    envs: updated
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- envs/refenv-xnvme-qemu.sh
+++ envs/refenv-xnvme-qemu.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-#
-# QEMU target environment skeleton
-#
-# The SSH configuration is overwritten such that everything using the "ssh::"
-# module will be targeted towards the QEMU guest.
-#
-# To access the QEMU host an explicit "QEMU_HOST" and utility functions are
-# provided by the "qemu::" module
-#
 
 # CIJOE: QEMU_* environment variables
 : "${QEMU_HOST:=localhost}"; export QEMU_HOST
@@ -31,38 +22,57 @@
 : "${SSH_USER:=root}"; export SSH_USER
 : "${SSH_NO_CHECKS:=1}"; export SSH_NO_CHECKS
 
+# xNVMe: the backend to utilize, this should be set in the testplan, but define
+# it here for running cijoe interactively
 #
+#: "${XNVME_ASYNC:=thr}"
+#: "${XNVME_ASYNC:=aio}"
+#: "${XNVME_ASYNC:=nil}"
+#: "${XNVME_ASYNC:=iou}"
+#: "${XNVME_BE:=linux}"
+#: "${XNVME_BE:=fbsd}"
+#: "${XNVME_BE:=spdk}"
+#
+
 # xNVMe: where are libraries and share stored on the target system? This is
 # needed to find the xNVMe fio io-engine, fio scripts etc.
 #
 : "${XNVME_LIB_ROOT:=/usr/lib}"; export XNVME_LIB_ROOT
 : "${XNVME_SHARE_ROOT:=/usr/share/xnvme}"; export XNVME_SHARE_ROOT
 
-#
 # xNVMe: which device is used for testing? set PCI_DEV_NAME, NVME_CNTID, and
 # NVME_NSID based on NVME_NSTYPE
 #
 if [[ -v NVME_NSTYPE ]]; then
+  : "${PCI_DEV_NAME=0000:03:00.0}"
+  : "${NVME_CNTID=0}"
+
   case $NVME_NSTYPE in
   lblk)
-    : "${PCI_DEV_NAME=0000:03:00.0}"
-    : "${NVME_CNTID=0}"
     : "${NVME_NSID=1}"
-    : "${NVME_DEV_NAME:=nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${NVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
     ;;
   zoned)
-    : "${PCI_DEV_NAME=0000:03:00.0}"
-    : "${NVME_CNTID=0}"
     : "${NVME_NSID=2}"
-    : "${NVME_DEV_NAME:=nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${NVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
     ;;
   *)
     echo "# ERROR: invalid NVME_NSTYPE(${NVME_NSTYPE})"
     exit 1
     ;;
   esac
+
+  if [[ -v DEV_TYPE && "${DEV_TYPE}" == "nullblk" ]]; then
+    case $NVME_NSTYPE in
+    lblk)
+      : "${NVME_DEV_NAME:=nullb0}"
+      ;;
+    zoned)
+      : "${NVME_DEV_NAME:=nullb1}"
+      ;;
+    esac
+  else
+    : "${NVME_DEV_NAME:=nvme${NVME_CNTID}n${NVME_NSID}}"
+  fi
+  : "${NVME_DEV_PATH:=/dev/${NVME_DEV_NAME}}"
 
   export PCI_DEV_NAME
   export NVME_NSID
@@ -72,53 +82,37 @@ if [[ -v NVME_NSTYPE ]]; then
 fi
 
 #
-# xNVMe: set XNVME_BE if not assigned
-# NOTE: This will be deprecated as the backends are now: LINUX, FBSD, and SPDK
+# xNVMe: define XNVME_URI and possibly HUGEMEM
 #
-#: "${XNVME_BE:=SPDK}"
-#: "${XNVME_BE:=FIOC}"
-#: "${XNVME_BE:=LIOC}"
-#: "${XNVME_BE:=LAIO}"
-#: "${XNVME_BE:=LIOU}"
-
-#
-# xNVMe: set XNVME_URI and possibly HUGEMEM
-#
-if [[ -v XNVME_BE && -v NVME_NSTYPE ]]; then
+if [[ -v XNVME_BE ]]; then
+  : "${XNVME_DEV_PATH:=${NVME_DEV_PATH}}"
 
   case $XNVME_BE in
-  LINUX|LIOU|LIOC)
-    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${XNVME_URI=${XNVME_DEV_PATH}?async=iou}"
-    ;;
-  LAIO)
-    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${XNVME_URI=${XNVME_DEV_PATH}?async=aio}"
-    ;;
-  NWRP)
-    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${XNVME_URI=${XNVME_DEV_PATH}?async=nil}"
-    ;;
-  FBSD|FREEBSD|FIOC)
-    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+  linux|fbsd)
     : "${XNVME_URI=${XNVME_DEV_PATH}}"
     ;;
-  SPDK)
-    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
-    : "${XNVME_URI=pci:${PCI_DEV_NAME}?nsid=${NVME_NSID}}"
-
-    # Set a default HUGEMEM that can be overwritten by testplan
+  spdk)
     : "${HUGEMEM:=4096}"
-    export HUGEMEM
+    : "${XNVME_URI=pci:${PCI_DEV_NAME}?nsid=${NVME_NSID}}"
     ;;
   *)
     echo "# ERROR: invalid XNVME_BE(${XNVME_BE})"
     exit 1
   esac
 
-  # Operating system device path
+  if [[ -v XNVME_ASYNC && "${XNVME_BE}" == "linux" ]]; then
+    case $XNVME_ASYNC in
+    thr|iou|aio|nil)
+      XNVME_URI="${XNVME_URI}?async=${XNVME_ASYNC}"
+      ;;
+    *)
+      echo "# ERROR: invalid XNVME_ASYNC(${XNVME_ASYNC})"
+      exit 1
+    esac
+  fi
+
+  export HUGEMEM
   export XNVME_DEV_PATH
-  # xNVMe URI
   export XNVME_URI
 fi
 
@@ -135,5 +129,4 @@ fi
 #
 # These are for the nullblock hook, specifically when loading it
 #
-: "${NULLBLK_QUEUE_MODE=2}"; export NULLBLK_QUEUE_MODE
-: "${NULLBLK_IRQMODE=0}"; export NULLBLK_IRQMODE
+: "${NULLBLK_NR_DEVICES=0}"; export NULLBLK_NR_DEVICES
