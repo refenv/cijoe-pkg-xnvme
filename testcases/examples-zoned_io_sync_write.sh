543c3e6ef9980fc820d7eee7ffe7354c821a0354
Simon A. F. Lund <simon.lund@samsung.com>
2020-10-04 08:51:55 +0200

    Fixup
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ testcases/examples-zoned_io_sync_write.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Verify that `zoned_io_sync write` runs without error
+#
+# Primitive check to verify that the example code is valid
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+: "${XNVME_URI:?Must be set and non-empty}"
+
+if ! ssh::cmd "zoned_io_sync write $XNVME_URI"; then
+  test::fail
+fi
+
+test::pass
