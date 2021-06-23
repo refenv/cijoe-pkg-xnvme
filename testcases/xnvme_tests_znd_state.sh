07a0298cd9a43e69fd800b7372b06bded03b8368
Simon A. F. Lund <simon.lund@samsung.com>
2021-06-23 20:42:59 +0200

    testcase: add wrapping of 'xnvme_tests_znd_state transition'
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ testcases/xnvme_tests_znd_state.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Verify that `xnvme_tests_znd_state state` runs without error
+#
+# This test requires that the device identified by $XNVME_URI transitions zone-states as defined in
+# the spec
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+: "${XNVME_URI:?Must be set and non-empty}"
+
+if ! cij::cmd "xnvme_tests_znd_state state $XNVME_URI"; then
+  test::fail
+fi
+
+test::pass
