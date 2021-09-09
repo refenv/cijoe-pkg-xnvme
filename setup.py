3ad1a1e4fcf00e3ee6859ea6e66af28e15b578aa
Simon A. F. Lund <simon.lund@samsung.com>
2021-09-09 15:56:33 +0200

    ver: bump to v0.0.25
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*parts):
 
 setup(
     name="cijoe-pkg-xnvme",
-    version="0.0.25.dev1",
+    version="0.0.25",
     description="CIJOE: xNVMe package",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
